6d6bf24d09f184e2ffc65469a440a8342c6efaaa
Ashis Raj <32839577+AshisRaj@users.noreply.github.com>
2025-05-14 22:29:33 +0530

    Update global-teardown.ts

--- src/utils/global-teardown.ts
+++ src/utils/global-teardown.ts
@@ -1,8 +1,16 @@
 import chalk from 'chalk';
+import { resolve } from 'path';
+import { rimraf } from 'rimraf';
 import logger from './logger';
 
 async function globalTeardown() {
   logger.info(chalk.blue('Global Teardown Started'));
+
+  const cookiestPath = resolve(__dirname, '../../artifacts/cookies');
+
+  // Cleanup old artifacts
+  rimraf(`${cookiestPath}`);
+  logger.info(chalk.green(`Deleted: ${cookiestPath}`));
 
   // Add your global teardown logic here
   logger.info(chalk.green('Global Teardown Completed'));
